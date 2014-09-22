028ce4f41e1aa7a1c60fb5af5dddaa819efd51af
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2014-09-22 15:16:40 +0200

    Project Start (java base classes)

---
+++ src/java/Pokephy/Database.java
@@ -0,0 +1,233 @@
+package Pokephy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Database {
+    /* *****************************************************************************************/
+
+    //Constructor
+    private Connection connexion;
+    
+    public Database(String url, String user, String password) {
+        //DEFINE JDBC objects for connection
+        try {
+            connexion = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to " + url + " successful for " + user);
+        } catch (SQLException ex) {
+            Logger.getLogger("ConnectBDD").log(Level.SEVERE, null, ex);
+            System.out.println("failed to connect to " + url + "  for " + user);
+        }
+    }
+
+    /* *****************************************************************************************/
+    /* JSP GETTERS AND SETTERS */ 
+    public Connection getConnexion() {
+        return connexion;
+    }
+    public void setConnexion(Connection connexion) {
+        this.connexion = connexion;
+    }
+    
+    /* CONVERSION METHODS */
+    //FORMAT CONVERTER
+    private static String extractString(ResultSet results, String fieldName) {
+        String field = "";
+        try {
+            if (results.getString(fieldName)!=null)
+                field = results.getString(fieldName).replaceAll("[ ]*$", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return field;
+    }
+    private static Integer extractNumber(ResultSet results, String fieldName) {
+        Integer field = null;
+        try {
+            if (results.getString(fieldName)!=null)
+                field = Integer.valueOf(results.getString(fieldName).replaceAll("[^0-9]*", ""));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return field;
+    }
+    private static Date extractDate(ResultSet results, String datename) {
+        Date date = null;
+        try {
+            //sql date 
+            java.sql.Date odate = results.getDate(datename);
+            //convert the oracle date to kernel.date
+            date = new Date(odate.getDay(), odate.getMonth(), odate.getYear());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+    private static String convertDateToString(Date date) {
+        return date.getDay() + "/" + date.getMonth() + "/" + date.getYear();
+    }
+    
+    //GLOBAL CODE 
+    private int getResultsCount(ResultSet results){
+        int rowCount = 0;
+        try {
+            while(results.next())
+                rowCount++;
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rowCount;
+    }
+    private ResultSet getResultsOfQuery(String query, boolean callNext){
+        ResultSet results = null;
+        try {
+            //Statement request = this.connexion.createStatement();
+            Statement request = this.getConnexion().createStatement();
+
+            //request all the objects from the database
+            results = request.executeQuery(query);
+            
+            //count the results if any
+            int rowCount = 0;
+            while(results.next())
+                rowCount++;
+            
+            //if any : reexecute and return
+            if (rowCount>0){
+                System.out.println(rowCount+" RESULTS FOUND FOR "+query);
+                results=request.executeQuery(query);
+                if (callNext)
+                    results.next();
+            }
+            else 
+                System.out.println("NO RESULTS FOUND FOR "+query);
+            
+            request.closeOnCompletion();
+        }
+        catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        return results;
+    }
+    private ResultSet getResultsOfQuery(String query){
+        return getResultsOfQuery(query,false);
+    }
+    
+    //PREPARED QUERIES
+    private ResultSet getResultSetFromIdQuery(String table, int id) {
+        ResultSet results;
+        table = table.toUpperCase();
+        String request = "SELECT * FROM table WHERE field = id";
+        request=request.replace("table", table);
+        request= request.replace("field", "ID_"+table);
+        request=request.replace("id", ""+id);
+        results = this.getResultsOfQuery(request,true);
+        return results;
+    }
+
+    /* INSERTION METHODS */
+    private void insertIntoTableAllValues(String table, Object... values){
+        try {
+            Statement s = this.connexion.createStatement();
+            
+            //create the query
+            String insert = "INSERT INTO "+table.toUpperCase()+" values(";
+            
+            for (int i = 0; i<values.length;i++)
+                insert+= values[i]+ (i==values.length-1 ? "" : ",");
+            
+            insert += ");";
+            
+            s.executeQuery(insert);
+            s.closeOnCompletion();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+    }//OBSOLETE : should not be used (see rules)
+    private int insertIntoTableValuesForFields(String table, String fields, Object...values){
+        String insert = "";
+        PreparedStatement statement = null;
+        Statement getIDGenerated = null;
+        ResultSet generatedID = null;
+        
+        int idGene = -1;
+        try {
+            //Statement s = this.connexion.createStatement();
+            
+            //create the query
+            insert = "INSERT INTO "+table.toUpperCase()+fields+" values(";
+            
+            for (int i = 0; i<values.length;i++)
+                insert+= (values[i] instanceof String ? "'" :"") + 
+                        values[i]+
+                        (values[i] instanceof String ? "'" :"")+
+                        (i==values.length-1 ? "": ","
+                        );
+            
+            insert += ")";
+            
+            System.out.println(insert);
+            
+            statement = this.connexion.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                System.out.println("ERROR : NO ROW AFFECTED");
+            }
+            
+            //GET THE LAST ID GENERERATED
+            getIDGenerated = this.connexion.createStatement();
+            insert = "SELECT MAX(ID_" + table.toUpperCase() + ") FROM "+table.toUpperCase()+"";
+            
+            generatedID = getIDGenerated.executeQuery(insert);
+            while(generatedID.next()) {
+                idGene = generatedID.getInt(1);
+            }
+            
+            statement.closeOnCompletion();
+            return idGene;
+            
+            //TODO: Find a better way to get back the generated ID...
+//            System.out.println("UpdateCount : " + statement.getUpdateCount());
+//            generatedKeys = statement.getGeneratedKeys();
+//            System.out.println("KEY GENERATED");
+//            int c = 0;
+//            while (generatedKeys.next()) {
+//                c++;
+//                //idGenerated = (int)generatedKeys.getInt(1);
+//                System.out.println("ID GENERATED : " + generatedKeys.getLong(c));
+//            }
+////            else {
+////                System.out.println("ERROR : NO ID GENERATED");
+////            }
+            
+        } catch (SQLException ex) {
+            System.out.println("ERROR ON : " + insert);
+            System.out.println(ex);
+            return -1;
+        }
+    }
+    
+    /* DELETION METHODS */
+    public void close() throws SQLException{
+        connexion.close();
+    }
+    
+/********************************************************************************************************************************************
+ *  PUBLIC QUERIES 
+ */
+    public void executeTestQuery()
+    {
+        ResultSet rs = this.getResultsOfQuery("SELECT * FROM OBJECT;");
+    }
+    
+}
+
--- src/java/Pokephy/Pokephy.java
+++ src/java/Pokephy/Pokephy.java
@@ -1,17 +1,160 @@
+package Pokephy;
+
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Pokephy;
-
 /**
  *
- * @author Maxime
+ * @author MrCake
  */
+
 public class Pokephy {
-        public static void main(String[] args) {
-        //Database db = new Database();
+    
+    /* USER/TRAINER DEFINITION */
+    public static class Trainer
+    {
+        private String name; 
+        public String getName(){ return name; }
+        
+        public ArrayList<Pokemon> pool = new ArrayList<>();
+        public Pokemon[] team = new Pokemon[6];
+    }
+    
+    /* POKEMON DEFINITION */
+    public static enum Type
+    {
+        Fire("FIRE"), Water("WATER"), Grass("GRASS");
+        
+        Type(String nameInDb){ nameInDatabase = nameInDb; }
+        
+        private final String nameInDatabase;
+        public String getName(){ return nameInDatabase ;}
+        
+        Type strongAgainst, weakAgainst;
+        
+        public static void init()
+        {
+            Fire.strongAgainst = Grass;
+            Fire.weakAgainst = Water;
+            
+            Water.strongAgainst = Fire;
+            Water.weakAgainst = Grass;
+            
+            Grass.strongAgainst = Water;
+            Grass.weakAgainst = Fire;
+        }
+    }
+    
+    public static class Skill
+    {
+        public static double defaultPower = 50.0;
+        
+        public enum SkillType
+        {
+            Physical, Special;
+        }
+        
+        public double power;
+        public SkillType skilltype;
+        public Type type;
+        
+        Skill(Type type, SkillType skilltype, double power)
+        {
+            this.type = type;
+            this.skilltype = skilltype;
+            this.power = power;
+        }
+        
+        public boolean isPhysical(){ return skilltype==SkillType.Physical; }
+        public boolean isSpecial(){ return skilltype==SkillType.Special; }
+    }
+    public static class PhysicalSkill extends Skill
+    {
+        public PhysicalSkill(Type type, double power) 
+        {
+            super(type, SkillType.Physical, power);
+        }
+    }
+    public static class SpecialSkill extends Skill
+    {
+        public SpecialSkill(Type type, double power) 
+        {
+            super(type, SkillType.Special, power);
+        }
+    }
+    
+    public static class Pokemon 
+    {
+        private double HP;
+        public double getHP(){ return HP; }
+        public void setHP(double hp){ this.HP = hp < 0 ? 0 : hp; }
+        
+        public boolean KO; 
+        public boolean isKO(){ return this.KO; }
+        
+        public double ATK, DEF, SP_ATK, SP_DEF, SPEED;
+        public double getATK(){ return ATK; }
+        public double getDEF(){ return DEF; }
+        public double getSPATK(){ return SP_ATK; }
+        public double getSPDEF(){ return SP_DEF; }
+        public double getSPEED(){ return SPEED; }
+        
+        private final String name;
+        public String getName(){ return name; }
+        
+        private final Type type;
+        public Type getType(){ return type; }
+        
+        private Trainer trainer;
+        public Trainer getTrainer(){ return trainer; }
+        
+        public PhysicalSkill physicalSkill;
+        public SpecialSkill specialSkill;
+        
+        Pokemon(String name, Type type, double HP, double ATK, double DEF, double SP_ATK, double SP_DEF, double SPEED)
+        {
+            this.name = name;
+            this.type = type;
+                this.physicalSkill = new PhysicalSkill(type,Skill.defaultPower);
+                this.specialSkill = new SpecialSkill(type,Skill.defaultPower);
+            
+            this.HP = HP;
+            this.ATK = ATK;
+            this.DEF = DEF;
+            this.SP_ATK = SP_ATK;
+            this.SP_DEF = SP_DEF;
+            this.SPEED = SPEED;
+        }
+        
+        /* FIGHT MANAGEMENT */
+        public void attack(Pokemon opponent, Skill skill)
+        {
+            //calculate modifiers
+            double type_bonus = 1.0;
+            double stab_bonus = 1.0;
+            
+            //calculate damage
+            double damage = (skill.isPhysical() ? ATK : SP_ATK) + skill.power; 
+            
+            //apply bonuses
+            damage *= skill.type == this.type ? 1.5 : 1.0; // STAB
+            damage *= skill.type == opponent.getType().weakAgainst ? 2.0 : 1.0;//Types balance
+            
+            //apply damage
+            opponent.setHP(opponent.getHP() - damage);
+        }
         
     }
+    
+    public static void main(String args[])
+    {
+        Database db = new Database("jdbc:mysql://localhost:3306/pokephy", "root", "");
+        db.executeTestQuery();
+    }
+    
 }
