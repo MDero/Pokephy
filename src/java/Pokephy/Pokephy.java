9d48459be462ddff587ab9111b270bfac994c46c
MDero <maxime.derobillard@gmail.com>
2014-09-22 15:12:01 +0200

    New sources

---
+++ src/java/Pokephy/Pokephy.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pokephy;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Pokephy {
+        public static void main(String[] args) {
+        //Database db = new Database();
+        
+    }
+}
